b233743439841c40bd49329c806119676e8744f0
vbondoux <vbondoux@gmail.com>
2025-01-02 14:09:02 +0100

    Update cron_task.py

--- cron_task.py
+++ cron_task.py
@@ -1,4 +1,9 @@
-from enrich_prompt import process_conversations
+import os
 
 if __name__ == "__main__":
-    process_conversations()
+    if os.getenv("RUN_CRON", "false").lower() == "true":
+        from enrich_prompt import process_conversations
+        process_conversations()
+    else:
+        from app import app
+        app.run(host="0.0.0.0", port=8080)
